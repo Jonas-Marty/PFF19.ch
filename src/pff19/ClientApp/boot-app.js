9e721dc07968a4401e646839858c160f694fd3b1
Lars Barmettler <barmettler.lars@gmail.com>
2018-06-25 23:43:41 +0200

    add font-awesome for icons

--- src/pff19/ClientApp/boot-app.js
+++ src/pff19/ClientApp/boot-app.js
@@ -2,6 +2,9 @@
 import 'core-js/es6/promise'
 import 'core-js/es6/array'
 
+window.$ = window.jQuery = require('jquery');
+require('bootstrap');
+
 import { app } from './app'
 
 app.$mount('#app')
--- src/pff19/webpack.config.js
+++ src/pff19/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = (env) => {
                         loader: 'file-loader',
                         options: {
                             name: '[name].[ext]',
-                            outputPath: 'fonts/'
+                            outputPath: 'fonts/',
                         }
                     }]
                 }
