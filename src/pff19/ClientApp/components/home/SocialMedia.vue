e73d22a52edbc09c2818f125d2590146d5ae5299
Lars Barmettler <barmettler.lars@gmail.com>
2018-06-26 17:10:56 +0200

    Design social media on home-page

--- src/pff19/ClientApp/components/home/SocialMedia.vue
+++ src/pff19/ClientApp/components/home/SocialMedia.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="social-media">
+        <img class="top-overlay" :src="require('assets/images/social_media_top_overlay.svg')"/>
         <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    sadf
-                </div>
-                <div class="col-4">
+            <div class="row flex-md-row-reverse">
+                <div class="col-md-4 col-12">
                     <h2>Social Media</h2>
+                </div>
+                <div class="col-md-8 col-12">
+                    sadf
                 </div>
             </div>
         </div>
+        <img class="bottom-overlay" :src="require('assets/images/social_media_bottom_overlay.svg')"/>
     </div>
 </template>
 
@@ -22,11 +24,25 @@ export default {
 <style lang="scss" scoped>
     .social-media {
         width: 100%;
-        height: 500px;
-        background: red;
-        margin-top: 150px;
-        padding: 50px 0 50px 0;
+        height: 800px;
+        background: #C20812;
+        margin-top: 50px;
+        padding: 150px 0 100px 0;
         color: white;
+        position: relative;
+        overflow: hidden;
+    }
+
+    .top-overlay {
+        position: absolute;
+        top: -1px;
+        right: -10px;
+    }
+
+    .bottom-overlay {
+        position: absolute;
+        bottom: -1px;
+        right: -10px;
     }
 </style>
 
