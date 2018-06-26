d37a6ed9f04297b5ea9b48f462e2ba0f36d0b421
Lars Barmettler <barmettler.lars@gmail.com>
2018-06-26 11:51:59 +0200

    color management when header is embedded an screensize is responsive

--- src/pff19/ClientApp/components/partials/Header.vue
+++ src/pff19/ClientApp/components/partials/Header.vue
@@ -1,20 +1,19 @@
 ﻿<template>
     <transition name="bounce">  
-        <nav class="navbar navbar-expand-md navbar-inverse position-fixed shadow-sm" :class="{embedded: embedded && isHome}">
-
+        <nav class="navbar navbar-expand-md navbar-inverse position-fixed shadow-sm" :class="{embedded: embedded && isHome && !navToggle}">
             <a class="navbar-brand" href="#" >
                 <img  :class="{}" src="../../assets/images/black_green_small_logo.png" height="40" class="d-inline-block align-top" alt="">
             </a>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="fa fa-navicon" style="color: white"></span>
+            <button class="navbar-toggler" @click="navToggle = !navToggle" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="fa fa-navicon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav ml-auto" >
                         <router-link
                             v-for="route in routes" :key="route.display"
-                            :to="route.name"
+                            :to="route.path"
                             class="nav-item nav-link"
                         > {{ route.display }}</router-link>
                      
@@ -33,7 +32,8 @@ export default {
         return {
             routes,
             collapsed : true,
-            embedded: true
+            embedded: true,
+            navToggle: false
         }
     },
     computed: {
@@ -69,6 +69,7 @@ export default {
     text-transform: uppercase;
 }
 
+
 .navbar, .shadow-sm {
     transition:all 0.500s ease;
     -webkit-transition:all 0.500s ease;
@@ -89,5 +90,11 @@ export default {
 .embedded .nav-item {
     color: white;
 }
+
+.embedded .navbar-toggler {
+    color: white;
+}
+
+
 
 </style>
--- src/pff19/ClientApp/routes.js
+++ src/pff19/ClientApp/routes.js
@@ -4,8 +4,8 @@ import HomePage from 'components/home/Home';
 import Sponsors from 'components/sponsors/SponsorList';
 
 export const routes = [
-    { path: '/', name: "home", component: HomePage, display: 'Home', style: 'glyphicon glyphicon-home' },
-    { path: '/counter', name: "counter", component: CounterExample, display: 'Counter', style: 'glyphicon glyphicon-education' },
-    { path: '/fetch-data', name: "fetchData", component: FetchData, display: 'Fetch data', style: 'glyphicon glyphicon-th-list' },
-    { path: '/sponsors', name: "sponsors", component: Sponsors, display: 'Sponsoren', style: 'glyphicon glyphicon-th-list' }
+    { path: '/', component: HomePage, display: 'Home', style: 'glyphicon glyphicon-home' },
+    { path: '/counter', component: CounterExample, display: 'Counter', style: 'glyphicon glyphicon-education' },
+    { path: '/fetch-data',component: FetchData, display: 'Fetch data', style: 'glyphicon glyphicon-th-list' },
+    { path: '/sponsors', component: Sponsors, display: 'Sponsoren', style: 'glyphicon glyphicon-th-list' }
 ]
