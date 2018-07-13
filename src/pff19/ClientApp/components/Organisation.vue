f4fb4abb1d58f73b772239a192c8c2e8840c7d6d
Lars Barmettler <barmettler.lars@gmail.com>
2018-07-13 12:19:54 +0200

    add Organisation text from pluto

--- src/pff19/ClientApp/components/Organisation.vue
+++ src/pff19/ClientApp/components/Organisation.vue
@@ -1,7 +1,20 @@
 <template>
     <div class="container">
         <h1>Organisation</h1>
-        <p>{{ $t('lang.components.button[1].signin') }}</p>
+        
+        <p>
+            Das PFF FFS 19 MOSAIK ist die Fortführung einer langjährigen, schweizweiten Tradition – dem Pfadi Folk Fest. 
+            Nach der Ausgabe im Sommer 2018 in Liestal findet das PFF im Sommer 2019 im Kanton Zürich statt. Vom 30. 
+            August – 01. September 2019  versammeln sich in Stäfa zahlreiche Pfadis und ihre Freunde unter dem Motto «MOSAIK», 
+            um ein unvergessliches Festival der etwas anderen Art zu geniessen. 
+        </p>
+        <p>
+            Jeder Teilnehmende bringt als Mosaikstein seine Ecken und Kanten ans PFF FFS 19 MOSAIK mit. Während dem ganzen 
+            Festival und den Konzerten bilden sich durch diese vielen verschiedenen anwesenden Mosaiksteinchen immer wieder 
+            neue Muster, Formen und Figuren. So erwarten wir im Sommer 2019 unzählig viele verschiedene Mosaikteilchen auf 
+            dem Festivalgelände, alle unterschiedlich und einzigartig. Sie vermischen sich, verändern sich gegenseitig und 
+            gehen mit neuen Erfahrungen und Eindrücken wieder auseinander.
+        </p>
     </div>
 </template>
 
@@ -15,6 +28,7 @@ export default {
 <style lang="scss" scoped>
     .container {
         padding-top: 100px;
+        min-height: 800px;
     }
 
 </style>
