74ce7d224f8b959d420ed8151ea0b7e040c42d71
Lars Barmettler <barmettler.lars@gmail.com>
2018-08-09 20:32:52 +0200

    add 404 error page

---
+++ src/pff19/ClientApp/components/partials/404.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+    <div class="row justify-content-center">
+         <img class="error-image" src="../../assets/images/404.png">
+    </div>
+</div>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .container {
+        padding-top: 100px;
+        min-height: 700px;
+    }
+
+    .error-image{
+        margin-top: 100px;
+    }
+</style>
+
