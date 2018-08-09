f3d185fa568afa63c4dbbe51d8761455bc201d0c
Lars Barmettler <barmettler.lars@gmail.com>
2018-08-09 19:37:47 +0200

    add facebook feed to home page

--- src/pff19/ClientApp/components/home/SocialMedia.vue
+++ src/pff19/ClientApp/components/home/SocialMedia.vue
@@ -7,8 +7,7 @@
                     <h2>Social Media</h2>
                 </div>
                 <div class="col-md-8 col-12">
-                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpff19mosaik%2F&tabs=timeline&width=0&height=0&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="0" height="0" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-                </div>
+<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpff19mosaik%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>                </div>
             </div>
         </div>
         <img class="bottom-overlay" :src="require('assets/images/social_media_bottom_overlay.svg')"/>
@@ -24,10 +23,10 @@ export default {
 <style lang="scss" scoped>
     .social-media {
         width: 100%;
-        height: 800px;
+        height: 950px;
         background: #C20812;
         margin-top: 50px;
-        padding: 150px 0 100px 0;
+        padding: 175px 0 100px 0;
         color: white;
         position: relative;
         overflow: hidden;
