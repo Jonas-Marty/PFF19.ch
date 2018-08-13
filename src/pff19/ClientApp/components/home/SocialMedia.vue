cb41f3301cf646a4b822c9581a99a36b92489d3f
Lars Barmettler <barmettler.lars@gmail.com>
2018-08-13 20:46:41 +0200

    fix graphical bug to social media svg overlays and add design pattern red

--- src/pff19/ClientApp/components/home/SocialMedia.vue
+++ src/pff19/ClientApp/components/home/SocialMedia.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="social-media">
             <img class="top-overlay" :src="require('assets/images/social_media_top_overlay.svg')"/>
+            <img class="right-overlay d-none d-xl-block" :src="require('assets/images/pattern_red_right.svg')"/>
         <div class="container">
             <div class="row flex-md-row-reverse">
                 <div class="col-md-4 col-12">
                     <h2>Social Media</h2>
                 </div>
-                <div class="col-md-8 col-12">
-<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpff19mosaik%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>                </div>
+                <div class="col-md-12 col-12">
+<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpff19mosaik%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>                </div>
             
             </div>
         </div>
@@ -37,9 +38,23 @@ export default {
         top: -2px;
     }
 
+    .right-overlay {
+        position: absolute;
+        width: 20%;
+        right: 0;
+        top: -30px;
+    }
+
     .bottom-overlay {
         position: absolute;
         bottom: -2px;
     }
+
+     @media (max-width: 768px) { 
+         .social-media {
+            padding: 100px 0 50px 0;
+            height: 800px;
+        }
+     }
 </style>
 
