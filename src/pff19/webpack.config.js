46ede51a1c34a1effdb76c7af2d242fcfbb03ffd
theMaimu <barmettler.lars@gmail.com>
2018-10-07 20:47:17 +0200

    fix production mode

--- src/pff19/webpack.config.js
+++ src/pff19/webpack.config.js
@@ -1,25 +1,32 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const bundleOutputDir = './wwwroot/dist';
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+const bundleOutputDir = './wwwroot/dist'
 
 module.exports = () => {
 
     console.log('Building for \x1b[33m%s\x1b[0m', process.env.NODE_ENV)
     const isDevBuild = !(process.env.NODE_ENV && process.env.NODE_ENV === 'production')
+    const extractCSS = new ExtractTextPlugin('site.css')
     return [{
         stats: { modules: false },
         entry: { 'main': './ClientApp/boot-app.js' },
         resolve: {
             extensions: ['.js', '.vue'],
-            alias: {
+            alias: isDevBuild ? {
                 'vue$': 'vue/dist/vue',
                 'components': path.resolve(__dirname, './ClientApp/components'),
                 'views': path.resolve(__dirname, './ClientApp/views'),
                 'utils': path.resolve(__dirname, './ClientApp/utils'),
                 'api': path.resolve(__dirname, './ClientApp/store/api'),
                 'assets': path.resolve(__dirname, './ClientApp/assets'),
-                'global_assets': path.resolve(__dirname, './assets')
+            } : {
+                'components': path.resolve(__dirname, './ClientApp/components'),
+                'views': path.resolve(__dirname, './ClientApp/views'),
+                'utils': path.resolve(__dirname, './ClientApp/utils'),
+                'api': path.resolve(__dirname, './ClientApp/store/api'),
+                'assets': path.resolve(__dirname, './ClientApp/assets'),
             }
         },
         output: {
@@ -52,19 +59,26 @@ module.exports = () => {
         },
         plugins: [
             new webpack.DllReferencePlugin({
-                context: __dirname,
-                manifest: require('./wwwroot/dist/vendor-manifest.json')
+              context: __dirname,
+              manifest: require('./wwwroot/dist/vendor-manifest.json')
             })
-        ].concat(isDevBuild ? [
+          ].concat(isDevBuild ? [
             // Plugins that apply in development builds only
             new webpack.SourceMapDevToolPlugin({
-                filename: '[file].map', // Remove this line if you prefer inline source maps
-                moduleFilenameTemplate: path.relative(bundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
+              filename: '[file].map', // Remove this line if you prefer inline source maps
+              moduleFilenameTemplate: path.relative(bundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
             })
-        ] : [
-                // Plugins that apply in production builds only
-                new webpack.optimize.UglifyJsPlugin(),
-                new ExtractTextPlugin('site.css')
-            ])
-    }];
+          ] : [
+            // Plugins that apply in production builds only
+            new webpack.optimize.UglifyJsPlugin(),
+            extractCSS,
+            // Compress extracted CSS.
+            new OptimizeCSSPlugin({
+              cssProcessorOptions: {
+                safe: true
+              }
+            })
+          ])
+        }]
 };
+
--- src/pff19/webpack.config.vendor.js
+++ src/pff19/webpack.config.vendor.js
@@ -1,7 +1,7 @@
-﻿const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+﻿const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = (env) => {
 
@@ -18,7 +18,7 @@ module.exports = (env) => {
         module: {
             rules: [
                 { test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, use: 'url-loader?limit=100000' },
-                { test: /\.css(\?|$)/, use: extractCSS.extract(['css-loader']) }
+                { test: /\.css(\?|$)/, use: extractCSS.extract(['css-loader']) },
             ]
         },
         entry: {
@@ -34,20 +34,25 @@ module.exports = (env) => {
             extractCSS,
             // Compress extracted CSS.
             new OptimizeCSSPlugin({
-                cssProcessorOptions: {
-                    safe: true
-                }
+              cssProcessorOptions: {
+                safe: true
+              }
             }),
-            new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
+            new webpack.ProvidePlugin({
+              $: 'jquery',
+              jQuery: 'jquery',
+              Popper: ['popper.js', 'default']
+              /* For modal, you will need to add tether */
+            }), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
             new webpack.DllPlugin({
-                path: path.join(__dirname, 'wwwroot', 'dist', '[name]-manifest.json'),
-                name: '[name]_[hash]'
+              path: path.join(__dirname, 'wwwroot', 'dist', '[name]-manifest.json'),
+              name: '[name]_[hash]'
             }),
             new webpack.DefinePlugin({
-                'process.env.NODE_ENV': isDevBuild ? '"development"' : '"production"'
+              'process.env.NODE_ENV': isDevBuild ? '"development"' : '"production"'
             })
-        ].concat(isDevBuild ? [] : [
+          ].concat(isDevBuild ? [] : [
             new webpack.optimize.UglifyJsPlugin()
-        ])
-    }];
+          ])
+        }]
 };
