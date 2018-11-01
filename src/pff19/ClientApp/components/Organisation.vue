c1a052d6c2f171802087920dd1b856766e766b99
theMaimu <barmettler.lars@gmail.com>
2018-11-01 13:34:20 +0100

    add member listing

--- src/pff19/ClientApp/components/Organisation.vue
+++ src/pff19/ClientApp/components/Organisation.vue
@@ -15,12 +15,200 @@
             dem Festivalgelände, alle unterschiedlich und einzigartig. Sie vermischen sich, verändern sich gegenseitig und 
             gehen mit neuen Erfahrungen und Eindrücken wieder auseinander.
         </p>
+        <div class="team">
+            <h2>Das Team</h2>
+            <div class="row members">
+                <div class="member col-6 col-md-4 col-lg-3" v-for="image in images" :key="image.scoutName">
+                    <div class="card">
+                        <img class="card-img-top" :src="require(`../assets/images/portraits/${image.scoutName}-min.jpg`)" :alt="image.scoutName">
+                        <div class="card-body">
+                            <p class="card-text"><b>{{image.scoutName}}</b>, {{image.name}}</p>
+                            <p class="card-text">{{image.email}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 
 export default {
+    data () {
+        return {
+            images: [
+                {   
+                    name: "Max Muster",
+                    scoutName: 'Aponi',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Auriga',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                    
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Baghira',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Bifidus',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Dwalin',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Fuego',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Goofy',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Hew',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Joya',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Karibu',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Lars Barmettler",
+                    scoutName: 'Maimu',
+                    ressort: 'Kommunikation',
+                    email: 'maimu@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Martin',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Momo',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Moneypenny',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Orisha',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Piccolo',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Pipistrella',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Pixel',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Pluto',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },{
+                    name: "Max Muster",
+                    scoutName: 'Rajko',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Ramon',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Snoopy',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Soraya',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Splash',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Strupf',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Varuna',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Wum',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                },
+                {
+                    name: "Max Muster",
+                    scoutName: 'Zirkel',
+                    ressort: 'Kommunikation',
+                    email: 'info@pff19.ch'
+                }
+            ]
+        }
+    }
 
 }
 </script>
@@ -29,6 +217,14 @@ export default {
     .container {
         padding-top: 100px;
         min-height: 800px;
+    }
+
+    .member {
+        margin-top: 30px;
+    }
+
+    .team {
+        margin-top: 50px;
     }
 
 </style>
