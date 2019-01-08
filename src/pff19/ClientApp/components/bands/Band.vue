12cffe6fd42df57d4322cfcd2b00ab638717a780
theMaimu <barmettler.lars@gmail.com>
2019-01-08 22:10:53 +0100

    create band view page

--- src/pff19/ClientApp/components/bands/Band.vue
+++ src/pff19/ClientApp/components/bands/Band.vue
@@ -13,11 +13,51 @@
                     </div>
                 </div>
                 
-                <h1>{{ this.getCurrentBand }}</h1>
-                <div class="date">{{ getCurrentBand.date | formateDate }}</div>
-                <br><br>
+                
+                <h1>{{ this.getCurrentBand.name }}</h1>
+                <div class="row" v-if="this.getCurrentBand.spotify">
+                    <div class="col-12 col-md-6 text-content" v-html="description"></div>
+                    <div class="col-12 col-md-6">
+                        <iframe 
+                            class="spotify"
+                            :src="`https://open.spotify.com/embed/user/themaimu/playlist/${this.getCurrentBand.spotify}`" 
+                            frameborder="0" 
+                            allowtransparency="true" 
+                            allow="encrypted-media">
+                        </iframe>
+                    </div>
+                </div>
+                <div class="row" v-else >
+                    <div class="col-12 text-content" v-html="description"></div>
+                </div>
+                                
+                <div class="row" v-if="this.getCurrentBand.webSiteUrl">
+                    <div class="col-12 band-website">
+                    <a :href="this.getCurrentBand.webSiteUrl">Band Website</a>
+                    </div>
+                </div>
+                <div class="row" v-if="this.getCurrentBand.facebook || this.getCurrentBand.instagram">
+                <div class="col-12 social-media-container">
+                    <div class="social-media" v-if="this.getCurrentBand.facebook">
+                        <a :href="this.getCurrentBand.facebook"> <img :src="require('assets/images/001-facebook.svg')" alt="facebook" class="social-icon"> </a>
+                    </div>
+                    <div class="social-media" v-if="this.getCurrentBand.instagram">
+                        <a :href="this.getCurrentBand.instagram"> <img :src="require('assets/images/003-instagram.svg')" alt="instagram" class="social-icon"> </a>
+                    </div>
+                </div>
+                </div>
 
-                <div class="text-content" v-html="newsContent"></div>
+
+                <div class="youtube-container row" v-if="videos">
+                    <div v-for="video in videos" :key="video" class=" col-12 col-md-6">
+                        <div class="iframe-container">
+                        <iframe class="ytplayer" type="text/html"
+                        :src="`http://www.youtube.com/embed/${video}`"
+                        frameborder="0"/>
+                        </div>
+                    </div>
+                </div>
+              
         </div>
     </div>
 </template>
@@ -30,7 +70,7 @@ import { mapGetters, mapActions } from 'vuex'
 export default {
     data () {
         return {
-            news: {}
+            bands: {},
         }
     },
 
@@ -49,13 +89,18 @@ export default {
             'getCurrentBand',
         ]),
 
-        newsContent () {
-            return this.$store.getters.language === 'de' ? this.getCurrentBand.contentDe : this.getCurrentBand.contentFr;
+        description () {
+            return this.$store.getters.language === 'de' ? this.getCurrentBand.descriptionDe : this.getCurrentBand.descriptionFr
         },
         getFirstImage () {
             if(this.getCurrentBand.images){
-                return this.getCurrentBand.images.split(';')[0];
+                return this.getCurrentBand.images.split(';')[0]
             }
+        },
+        videos (){
+            if(this.getCurrentBand.youtubeUrls) {
+                return  this.getCurrentBand.youtubeUrls.replace(/\s/g, '').split(',')
+            } 
         }
         
     }
@@ -67,7 +112,7 @@ export default {
         margin-bottom: 20px;
     }
     .container {
-        padding-top: 100px; 
+        padding-top: 50px; 
     }
 
     .img {
@@ -98,6 +143,52 @@ export default {
         background-position: center;
         position: relative;
         overflow: hidden;
+    }
+
+    .ytplayer {
+        width: 100%;
+        height: 300px;
+    }
+
+    .youtube-container{
+        margin-top: 50px 
+    }
+
+    .social-media{
+        float: left;
+        margin-right: 20px;
+    }
+
+    .social-icon {
+        width: 40px;
+    }  
+
+    .social-media-wrapper{
+        width: 100%;
+    }
+    .social-media-container{
+        margin: 20px 20px 20px 0;
+    }
+
+    .spotify {
+        float: right;
+        width: 100%;
+        max-width: 340px;
+        height: 400px;
+    }
+
+    .band-website {
+        margin: 20px 0 20px 0;
+    }
+
+
+    @media (max-width: 768px) { 
+        .spotify {
+            float:left;
+        }
+        .text-content {
+            margin-bottom: 20px;
+        }
     }
 </style>
 
