74c8632c78f4f1779b776d858a343f4e6077cc69
Jonas Marty <jonasmarty@hotmail.com>
2019-01-19 13:49:00 +0100

    playtime of band is now nullable. Also on FAQ made Question and Answer De & Fr and Category Null

---
+++ src/pff19.DataAccess/Migrations/20190119124702_ChangeBandPlayTimeToStringColumn.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pff19.DataAccess.Migrations
+{
+    public partial class ChangeBandPlayTimeToStringColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestionFr",
+                table: "Faqs",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestionDe",
+                table: "Faqs",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Faqs",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AnswerFr",
+                table: "Faqs",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AnswerDe",
+                table: "Faqs",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PlayTime",
+                table: "Bands",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestionFr",
+                table: "Faqs",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestionDe",
+                table: "Faqs",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Faqs",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AnswerFr",
+                table: "Faqs",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AnswerDe",
+                table: "Faqs",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PlayTime",
+                table: "Bands",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
