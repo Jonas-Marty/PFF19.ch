5b9a14574a171df536e43d2fd5d0a640680cb15a
theMaimu <barmettler.lars@gmail.com>
2019-01-21 22:02:22 +0100

    add helfertool link on helferpage

--- src/pff19/ClientApp/components/helper/TextFr.vue
+++ src/pff19/ClientApp/components/helper/TextFr.vue
@@ -1,8 +1,27 @@
 <template>
      <p>
-       Intéresses-tu à contribuer au FFS comme bénévole? Alors remplis le formulaire suivant. Comme ça
+        Intéresses-tu à contribuer au FFS comme bénévole? Alors remplis le formulaire suivant. Comme ça
         nous pouvons t’informer avant tous les autres, quand nous mettons le bénévoles-tool online (ca.
         printemps 2019). Attention : Ce n’est pas une inscription définitive. Adresse-toi avec des questions
         générales à <a href="mailto:info@pff19.ch">info@pff19.ch</a>.
     </p>
 </template>
+
+<template>
+    <div class="info-text">
+        <p>
+            Intéresses-tu à contribuer au FFS comme bénévole? Alors remplis le formulaire suivant. Comme ça
+            nous pouvons t’informer avant tous les autres, quand nous mettons le bénévoles-tool online (ca.
+            printemps 2019). Attention : Ce n’est pas une inscription définitive. Adresse-toi avec des questions
+            générales à <a href="mailto:info@pff19.ch">info@pff19.ch</a>.
+        </p>
+        <a href="https://pff19.ukula.ch/"><div class="btn btn-primary">Bénévoles-Tool</div></a>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.info-text {
+    margin-bottom: 50px;
+}
+</style>
+
