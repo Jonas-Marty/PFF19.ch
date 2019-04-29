da55991f5542eba114b0c166990f26da85cb787e
Lars Barmettler <barmettler.lars@gmail.cokm>
2019-04-29 20:54:08 +0200

    make description language based

--- src/pff19/ClientApp/layouts/Default.vue
+++ src/pff19/ClientApp/layouts/Default.vue
@@ -16,7 +16,15 @@ export default {
         return {
             titleTemplate: 'PFF FFS 19 %s',
             htmlAttrs: {
-                lang: this.language
+                lang: this.language,
+                description:
+                    this.language === 'de'
+                        ? `«Mosaik» ist das Motto des PFF, das vom 30. August – 01. 
+                            September 2019 in Stäfa (ZH) stattfindet. Während 3 Tagen versammeln sich zahlreiche Pfadis und 
+                            ihre Freunde, um ein Openair der etwas anderen Art zu geniessen.`
+                        : `« Mosaïque » c’est la devise du FFS, qui a lieu à Stäfa (ZH) du 30 août au 1er 
+                            septembre 2019. Au cours de  3 jours nombreux scouts et leurs amis regroupent pour 
+                            profiter d’un festival en plein air inoubliable.`
             }
         }
     },
