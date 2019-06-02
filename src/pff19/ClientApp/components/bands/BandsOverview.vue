dc7dbb197ad029223759adbda8eb8903072297d1
Lars Barmettler <barmettler.lars@gmail.cokm>
2019-06-02 18:35:31 +0200

    update bands overview responsiveness

--- src/pff19/ClientApp/components/bands/BandsOverview.vue
+++ src/pff19/ClientApp/components/bands/BandsOverview.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-9">
+            <div class="col-md-8 col-lg-9">
                 <h1>{{ $t(`lang.components.bands.title`) }}</h1>
             </div>
-            <div class="col-3">
+            <div class="col-md-4 col-lg-3">
                 <form>
                     <div class="form-group">
                         <select id="days" class="form-control" v-model="selectedDay">
