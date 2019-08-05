ec1e6f44dae6de7416ef9085664fc01256d06e94
Lars Barmettler <barmettler.lars@gmail.cokm>
2019-08-05 19:19:16 +0200

    add sbb rabatt code link to ticketing page

--- src/pff19/ClientApp/components/ticketing/DescriptionDe.vue
+++ src/pff19/ClientApp/components/ticketing/DescriptionDe.vue
@@ -7,7 +7,11 @@
         <h3>Helfer</h3>
         <p>
           Möchtest Du das PFF als Helfer unterstützen und Dir dadurch einen Rabatt auf Dein Ticket
-          sichern?<a href="https://pff19.ukula.ch/" target="_blank"> Werde Helfer!</a>
+          sichern?
+          <a
+            href="https://pff19.ukula.ch/"
+            target="_blank"
+          >Werde Helfer!</a>
           (Helfer beziehen ihr vergünstigtes Ticket direkt an der Kasse vor Ort und müssen kein
           Ticket im Vorverkauf kaufen!)
         </p>
@@ -16,11 +20,10 @@
       <div class="col-md-6 offset-md-3 iframe-wrapper">
         <iframe
           class="iframe"
-          datasrc="https://www.ticketino.com/de/Event/Embed/80740"
+          data-src="https://www.ticketino.com/de/Event/Embed/80740"
           frameborder="0"
           src="https://www.ticketino.com/de/Event/Embed/80740"
-        >
-        </iframe>
+        ></iframe>
       </div>
     </div>
 
@@ -28,23 +31,28 @@
       <h3>Infos</h3>
       <ul>
         <li>
-          Beim Kauf eines 3-Tagespasses bis zum <b>31. Mai 2019</b> erhält man
-          <b>50% Vergünstigung</b> auf das Ticket für die Hin- und Rückfahrt (SBB Rabatt Code).
+          Beim Kauf eines 3-Tagespasses bis zum
+          <b>31. Mai 2019</b> erhält man
+          <b>50% Vergünstigung</b> auf das Ticket für die Hin- und Rückfahrt (
+          <a
+            href="https://sbb.pff19.ch/"
+          >SBB Rabatt Code</a> ).
         </li>
 
-        <li><b>Vor Ort</b> können am Sonntag Einzeltickets gekauft werden</li>
+        <li>
+          <b>Vor Ort</b> können am Sonntag Einzeltickets gekauft werden
+        </li>
 
         <li>
           <b>Familienticket</b> enthält Eintritt für 1 Familie (2 Erwachsene + ihre Kinder bis 16
           Jahre). Andere Tickets für den Sonntag können nicht im Vorverkauf gekauft werden. Kinder
-          unter 6 Jahre sind <b>gratis</b>.
+          unter 6 Jahre sind
+          <b>gratis</b>.
         </li>
 
         <li>Das PFF 19 ist für Besucher ab 16 Jahren. (Ausser am Familiensonntag)</li>
 
-        <li>
-          Bitte beachte, dass Glasflaschen auf dem Festivalgelände und dem Zeltplatz verboten sind.
-        </li>
+        <li>Bitte beachte, dass Glasflaschen auf dem Festivalgelände und dem Zeltplatz verboten sind.</li>
 
         <li>
           Gebrannter Alkohol ist am gesamten Festival nicht erlaubt. Er wird nicht toleriert und
@@ -53,9 +61,10 @@
 
         <li>
           Mit dem Kauf eines Tickets akzeptierst du die
-          <a href="assets/AGB PFF Stäfa 2019 Besucher.pdf" target="_blank"
-            >Allgemeinen Geschäftsbedingungen</a
-          >
+          <a
+            href="assets/AGB PFF Stäfa 2019 Besucher.pdf"
+            target="_blank"
+          >Allgemeinen Geschäftsbedingungen</a>
           (AGB) des Vereins PFF Stäfa 2019.
         </li>
 
@@ -66,38 +75,35 @@
       </ul>
       <p>
         Noch Fragen zu den Tickets oder zum Festival? Die Antwort findest du bestimmt
-        <router-link :to="{ name: 'faq' }"> hier</router-link>.
+        <router-link :to="{ name: 'faq' }">hier</router-link>.
       </p>
       <p>
         Hast du technische Probleme beim Ticketkauf oder dem Zahlungsprozess? Gerne helfen wir Dir
-        weiter unter <a href="mailto:ticket@pff19.ch">ticket@pff19.ch</a>
+        weiter unter
+        <a
+          href="mailto:ticket@pff19.ch"
+        >ticket@pff19.ch</a>
       </p>
     </div>
 
     <div class="row">
       <div class="col-12 col-md-4">
         <div class="card">
-          <img
-            class="card-img-top"
-            src="../../assets/images/3_tages_pass.jpg"
-            alt="Card image cap"
-          />
+          <img class="card-img-top" src="../../assets/images/3_tages_pass.jpg" alt="Card image cap" />
           <div class="card-body">
             <h5 class="card-title">3-Tagespass</h5>
             <ul class="list-group list-group-flush">
-              <li class="list-group-item">
-                Zutritt zum Festivalgelände und zum Zeltplatz von Freitag bis Sonntag
-              </li>
+              <li
+                class="list-group-item"
+              >Zutritt zum Festivalgelände und zum Zeltplatz von Freitag bis Sonntag</li>
               <li class="list-group-item">Goodie-Bag inkl. 1x Festivalbecher</li>
-              <li class="list-group-item">
-                Verpflegung: 2x Abend, 2x Morgen. Bitte bring Dein Essgeschirr mit.
-              </li>
-              <li class="list-group-item">
-                Shuttlebus vom Bahnhof Stäfa zum Festivalgelände
-              </li>
-              <li class="list-group-item">
-                Vergünstigte Anreise mit SBB beim Kauf eines Tickets bis 31. Mai 2019
-              </li>
+              <li
+                class="list-group-item"
+              >Verpflegung: 2x Abend, 2x Morgen. Bitte bring Dein Essgeschirr mit.</li>
+              <li class="list-group-item">Shuttlebus vom Bahnhof Stäfa zum Festivalgelände</li>
+              <li
+                class="list-group-item"
+              >Vergünstigte Anreise mit SBB beim Kauf eines Tickets bis 31. Mai 2019</li>
             </ul>
           </div>
         </div>
@@ -113,16 +119,10 @@
           <div class="card-body">
             <h5 class="card-title">Tageseintritt Freitag oder Samstag</h5>
             <ul class="list-group list-group-flush">
-              <li class="list-group-item">
-                Zutritt zum Festivalgelände am jeweiligen Tag
-              </li>
+              <li class="list-group-item">Zutritt zum Festivalgelände am jeweiligen Tag</li>
               <li class="list-group-item">1x Festivalbecher</li>
-              <li class="list-group-item">
-                Verpflegung: 1x Abend. Bitte bring Dein Essgeschirr mit.
-              </li>
-              <li class="list-group-item">
-                Shuttlebus vom Bahnhof Stäfa zum Festivalgelände
-              </li>
+              <li class="list-group-item">Verpflegung: 1x Abend. Bitte bring Dein Essgeschirr mit.</li>
+              <li class="list-group-item">Shuttlebus vom Bahnhof Stäfa zum Festivalgelände</li>
             </ul>
           </div>
         </div>
@@ -143,16 +143,14 @@
                 Jahre) sowie Teilnahme am Programm für Kinder
               </li>
               <li class="list-group-item">2x Festivalbecher</li>
-              <li class="list-group-item">
-                Shuttlebus vom Bahnhof Stäfa zum Festivalgelände
-              </li>
+              <li class="list-group-item">Shuttlebus vom Bahnhof Stäfa zum Festivalgelände</li>
             </ul>
           </div>
         </div>
       </div>
     </div>
 
-    <app-polygon color="yellow" position="left"> </app-polygon>
+    <app-polygon color="yellow" position="left"></app-polygon>
   </div>
 </template>
 
--- src/pff19/ClientApp/components/ticketing/DescriptionFr.vue
+++ src/pff19/ClientApp/components/ticketing/DescriptionFr.vue
@@ -8,7 +8,10 @@
         <p>
           Est-ce que tu voudrais soutenir le FFS comme bénévole et ainsi profiter d’un avantage sur
           ton ticket?
-          <a href="https://pff19.ukula.ch/" target="_blank">Deviens bénévole!</a> (Les bénévoles
+          <a
+            href="https://pff19.ukula.ch/"
+            target="_blank"
+          >Deviens bénévole!</a> (Les bénévoles
           reçoivent leurs tickets réduits directement à la caisse quand ils arrivent et ils ne
           doivent pas acheter un ticket en prévente!)
         </p>
@@ -17,11 +20,10 @@
       <div class="col-md-6 offset-md-3 iframe-wrapper">
         <iframe
           class="iframe"
-          datasrc="https://www.ticketino.com/fr/Event/Embed/80740"
+          data-src="https://www.ticketino.com/fr/Event/Embed/80740"
           frameborder="0"
           src="https://www.ticketino.com/fr/Event/Embed/80740"
-        >
-        </iframe>
+        ></iframe>
       </div>
     </div>
 
@@ -29,24 +31,26 @@
       <h3>Informations</h3>
       <ul>
         <li>
-          Quand tu achètes un ticket de trois jours avant le <b>31. mai</b>, tu reçois une réduction
-          de <b>50%</b> pour ton ticket de CFF pour aller et retour en train (CFF code de
-          réduction).
+          Quand tu achètes un ticket de trois jours avant le
+          <b>31. mai</b>, tu reçois une réduction
+          de
+          <b>50%</b> pour ton ticket de CFF pour aller et retour en train (
+          <a href="https://sbb.pff19.ch/">
+            CFF code de
+            réduction
+          </a> ).
         </li>
 
-        <li>
-          Il est possible d’acheter un ticket (pour une personne) le dimanche à la caisse.
-        </li>
+        <li>Il est possible d’acheter un ticket (pour une personne) le dimanche à la caisse.</li>
 
         <li>
           <b>Le ticket de familles</b> contient l’entrée pour une famille (2 adultes + leurs enfants
           jusqu’à l’âge 16). Il n’est pas possible d’acheter d’autres tickets pour le dimanche en
-          prévente. Enfants de moins de 6 ans sont <b>gratuits</b>.
+          prévente. Enfants de moins de 6 ans sont
+          <b>gratuits</b>.
         </li>
 
-        <li>
-          Le FFS est pour les visiteurs à partir de 16 ans (sauf le dimanche de familles).
-        </li>
+        <li>Le FFS est pour les visiteurs à partir de 16 ans (sauf le dimanche de familles).</li>
 
         <li>
           Attention: Les bouteilles de verre sont interdites au terrain du festival et à
@@ -60,7 +64,10 @@
 
         <li>
           Avec l'achat d'un ticket ou avec l'inscription comme bénévole tu acceptes nos
-          <a href="/assets/AGB PFF Stäfa 2019 Besucher.pdf" target="_blank">conditions générales</a>
+          <a
+            href="/assets/AGB PFF Stäfa 2019 Besucher.pdf"
+            target="_blank"
+          >conditions générales</a>
         </li>
 
         <li>
@@ -70,40 +77,37 @@
       </ul>
       <p>
         Est-ce que tu as des questions au sujet des tickets ou du festival? Tu trouveras
-        certainement les réponses <router-link :to="{ name: 'faq' }">ici</router-link>.
+        certainement les réponses
+        <router-link :to="{ name: 'faq' }">ici</router-link>.
       </p>
       <p>
         Est-ce que tu as de problèmes techniques avec l’achat de tickets ou avec le processus de
         paiement? Nous sommes à ton disposition pour t’aider sous
-        <a href="mailto:ticket@pff19.ch">ticket@pff19.ch</a>
+        <a
+          href="mailto:ticket@pff19.ch"
+        >ticket@pff19.ch</a>
       </p>
     </div>
 
     <div class="row">
       <div class="col-12 col-md-4">
         <div class="card">
-          <img
-            class="card-img-top"
-            src="../../assets/images/3_tages_pass.jpg"
-            alt="Card image cap"
-          />
+          <img class="card-img-top" src="../../assets/images/3_tages_pass.jpg" alt="Card image cap" />
           <div class="card-body">
             <h5 class="card-title">Ticket de 3 jours</h5>
             <ul class="list-group list-group-flush">
-              <li class="list-group-item">
-                Entrée au terrain du festival et à l’emplacement de tentes du vendredi au dimanche
-              </li>
+              <li
+                class="list-group-item"
+              >Entrée au terrain du festival et à l’emplacement de tentes du vendredi au dimanche</li>
               <li class="list-group-item">Goodie-bag incl. 1x gobelet de festival</li>
               <li class="list-group-item">
                 Ravitaillement : 2x dîner, 2x petit-déjeuner. S’il te plaît amène ta propre
                 vaisselle pour manger.
               </li>
-              <li class="list-group-item">
-                Navette de la gare Stäfa au terrain du festival
-              </li>
-              <li class="list-group-item">
-                Voyage au prix réduit avec le CFF quand tu achètes un ticket avant le 31. mai 2019
-              </li>
+              <li class="list-group-item">Navette de la gare Stäfa au terrain du festival</li>
+              <li
+                class="list-group-item"
+              >Voyage au prix réduit avec le CFF quand tu achètes un ticket avant le 31. mai 2019</li>
             </ul>
           </div>
         </div>
@@ -119,16 +123,12 @@
           <div class="card-body">
             <h5 class="card-title">Ticket pour 1 jour vendredi ou samedi</h5>
             <ul class="list-group list-group-flush">
-              <li class="list-group-item">
-                Entrée au terrain du festival au jour respectif
-              </li>
+              <li class="list-group-item">Entrée au terrain du festival au jour respectif</li>
               <li class="list-group-item">1x gobelet de festival</li>
-              <li class="list-group-item">
-                Ravitaillement : 1x dîner. S’il te plaît amène ta propre vaisselle pour manger.
-              </li>
-              <li class="list-group-item">
-                Navette de la gare Stäfa au terrain du festival
-              </li>
+              <li
+                class="list-group-item"
+              >Ravitaillement : 1x dîner. S’il te plaît amène ta propre vaisselle pour manger.</li>
+              <li class="list-group-item">Navette de la gare Stäfa au terrain du festival</li>
             </ul>
           </div>
         </div>
@@ -149,16 +149,14 @@
                 jusqu’à l’âge 16) et participation au programme pour les enfants
               </li>
               <li class="list-group-item">2x gobelets de festival</li>
-              <li class="list-group-item">
-                Navette de la gare Stäfa au terrain du festival
-              </li>
+              <li class="list-group-item">Navette de la gare Stäfa au terrain du festival</li>
             </ul>
           </div>
         </div>
       </div>
     </div>
 
-    <app-polygon color="yellow" position="left"> </app-polygon>
+    <app-polygon color="yellow" position="left"></app-polygon>
   </div>
 </template>
 
